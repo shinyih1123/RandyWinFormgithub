a11b081ce83a14a24ad87927d5a56bb7d953af4b
Randy Tsai <shinyih1123@gmail.com>
2024-07-24 11:40:51 +0800

    Homework3_GuessNumber

--- RandyWinFormsApp1/Form1.Designer.cs
+++ RandyWinFormsApp1/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace RandyWinFormsApp1
+﻿using Microsoft.VisualBasic;
+
+namespace RandyWinFormsApp1
 {
     partial class Form1
     {
@@ -28,12 +30,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonNew = new Button();
+            buttonQuit = new Button();
+            label = new Label();
+            textBoxInput = new TextBox();
+            buttonGuess = new Button();
+            labelNo = new Label();
+            labelGuess = new Label();
+            labelResult = new Label();
+            label1 = new Label();
+            buttonGiveUp = new Button();
+            textBoxResult = new TextBox();
+            SuspendLayout();
+            // 
+            // buttonNew
+            // 
+            buttonNew.Location = new Point(12, 12);
+            buttonNew.Name = "buttonNew";
+            buttonNew.Size = new Size(99, 45);
+            buttonNew.TabIndex = 0;
+            buttonNew.Text = "New Game";
+            buttonNew.UseVisualStyleBackColor = true;
+            buttonNew.Click += buttonNew_Click;
+            // 
+            // buttonQuit
+            // 
+            buttonQuit.Location = new Point(186, 12);
+            buttonQuit.Name = "buttonQuit";
+            buttonQuit.Size = new Size(99, 46);
+            buttonQuit.TabIndex = 1;
+            buttonQuit.Text = "Quit";
+            buttonQuit.UseVisualStyleBackColor = true;
+            buttonQuit.Click += buttonQuit_Click;
+            // 
+            // label
+            // 
+            label.AutoSize = true;
+            label.Location = new Point(13, 97);
+            label.Name = "label";
+            label.Size = new Size(39, 19);
+            label.TabIndex = 2;
+            label.Text = "輸入";
+            // 
+            // textBoxInput
+            // 
+            textBoxInput.Enabled = false;
+            textBoxInput.Location = new Point(58, 94);
+            textBoxInput.MaxLength = 4;
+            textBoxInput.Name = "textBoxInput";
+            textBoxInput.Size = new Size(111, 27);
+            textBoxInput.TabIndex = 3;
+            // 
+            // buttonGuess
+            // 
+            buttonGuess.Location = new Point(175, 92);
+            buttonGuess.Enabled = false;
+            buttonGuess.Name = "buttonGuess";
+            buttonGuess.Size = new Size(54, 29);
+            buttonGuess.TabIndex = 4;
+            buttonGuess.Text = "OK";
+            buttonGuess.UseVisualStyleBackColor = true;
+            buttonGuess.Click += buttonGuess_Click;
+            // 
+            // labelNo
+            // 
+            labelNo.AutoSize = true;
+            labelNo.Location = new Point(13, 131);
+            labelNo.Name = "labelNo";
+            labelNo.Size = new Size(33, 19);
+            labelNo.TabIndex = 5;
+            labelNo.Text = "No.";
+            // 
+            // labelGuess
+            // 
+            labelGuess.AutoSize = true;
+            labelGuess.Location = new Point(77, 131);
+            labelGuess.Name = "labelGuess";
+            labelGuess.Size = new Size(51, 19);
+            labelGuess.TabIndex = 6;
+            labelGuess.Text = "Guess";
+            // 
+            // labelResult
+            // 
+            labelResult.AutoSize = true;
+            labelResult.Location = new Point(175, 131);
+            labelResult.Name = "labelResult";
+            labelResult.Size = new Size(52, 19);
+            labelResult.TabIndex = 7;
+            labelResult.Text = "Result";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(174, 19);
+            label1.TabIndex = 8;
+            label1.Text = "猜一個四位不重複的數字";
+            // 
+            // buttonGiveUp
+            // 
+            buttonGiveUp.Enabled = false;
+            buttonGiveUp.Location = new Point(216, 429);
+            buttonGiveUp.Name = "buttonGiveUp";
+            buttonGiveUp.Size = new Size(69, 29);
+            buttonGiveUp.TabIndex = 10;
+            buttonGiveUp.Text = "GiveUp";
+            buttonGiveUp.UseVisualStyleBackColor = true;
+            buttonGiveUp.Click += buttonGiveUp_Click;
+            // 
+            // textBoxResult
+            // 
+            textBoxResult.Enabled = false;
+            textBoxResult.Location = new Point(13, 153);
+            textBoxResult.Multiline = true;
+            textBoxResult.Name = "textBoxResult";
+            textBoxResult.Size = new Size(272, 270);
+            textBoxResult.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(305, 473);
+            Controls.Add(textBoxResult);
+            Controls.Add(buttonGiveUp);
+            Controls.Add(label1);
+            Controls.Add(labelResult);
+            Controls.Add(labelGuess);
+            Controls.Add(labelNo);
+            Controls.Add(buttonGuess);
+            Controls.Add(textBoxInput);
+            Controls.Add(label);
+            Controls.Add(buttonQuit);
+            Controls.Add(buttonNew);
+            Name = "Form1";
+            Text = "1A2B猜數字遊戲";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button buttonNew;
+        private Button buttonQuit;
+        private Label label;
+        private TextBox textBoxInput;
+        private Button buttonGuess;
+        private Label labelNo;
+        private Label labelGuess;
+        private Label labelResult;
+        private Label label1;
+        private Button buttonGiveUp;
+        private TextBox textBoxResult;
     }
 }
--- RandyWinFormsApp1/Form1.cs
+++ RandyWinFormsApp1/Form1.cs
@@ -2,9 +2,112 @@ namespace RandyWinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private int[] ans = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };//正確答案
+        private String[] gnum = new string[10];//猜的答案數字
+        private int tmp, r ,time;
+        private Random ran = new Random();
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            textBoxInput.Enabled = true;
+            textBoxResult.Text = "";
+            textBoxInput.Text = "";
+            time = 0;
+            buttonGuess.Enabled = true;
+            buttonGiveUp.Enabled = true;
+            for (int i = 0; i < 10; i++)
+            {
+                ans[i] = i;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                r = ran.Next(0, 10);
+                tmp = ans[r];
+                ans[r] = ans[9 - i];
+                ans[9 - i] = tmp;
+            }
+        }
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonGuess_Click(object sender, EventArgs e)
+        {
+            string num = "";
+            time = time + 1;
+            int a = 0, b = 0;
+            if (textBoxInput.TextLength != 4)
+            {
+                MessageBox.Show("請輸入4個不一樣的數字");
+            }
+            else
+            {
+                for (int j = 1; j <= 4; j++)
+                {
+                    gnum[j] = textBoxInput.Text.Substring(j - 1, 1);
+                    num += gnum[j];
+                }
+                if ((gnum[1] == gnum[2] || gnum[1] == gnum[3] ||
+                gnum[1] == gnum[4] || gnum[2] == gnum[3] ||
+                gnum[2] == gnum[4] || gnum[3] == gnum[4]))
+                {
+                    MessageBox.Show("不能輸入重複的數字哦");
+                }
+                else
+                {
+                    for (int k = 1; k <= 4; k++)
+                    {
+                        for (int l = 1; l <= 4; l++)
+                        {
+                            if (gnum[k] == ans[l].ToString())
+                            {
+                                if (k == l)
+                                {
+                                    a++;
+                                }
+                                else if (k != l)
+                                {
+                                    b++;
+                                }
+                            }
+                        }
+                    }
+                    textBoxResult.Text += time +"---------"+num + "----------" + a.ToString() + "A" + b.ToString() + "B" + "\r\n";
+                    //label3.Text = "第" + (listBoxResult.Lines.Length - 1) + "猜";
+                    textBoxInput.Focus();
+                    textBoxInput.SelectAll();
+                }
+                if (a == 4 && b == 0)
+                {
+                    MessageBox.Show("恭喜你猜對了");
+                    textBoxInput.Enabled = false;
+                }
+                else if (textBoxResult.Lines.Length == 11)
+                {
+                    MessageBox.Show("你要再加油了");
+                    textBoxInput.Enabled = false;
+                }
+            }
+        }
+
+        private void buttonGiveUp_Click(object sender, EventArgs e)
+        {
+            //textBoxResult.Text = "";
+            textBoxResult.Text += "Ans===" + ans[1] + ans[2] + ans[3] + ans[4] + "===="+"\r\n";
+            textBoxInput.Enabled = false;
+            buttonGuess.Enabled = false;
+            buttonGiveUp.Enabled = false;
+            //for (int i = 1; i <= 4; i++)
+            //{
+            //    textBoxResult.Text += ans[i];
+            //}
         }
     }
 }
